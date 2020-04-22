d44100be389148c2fb53c9f137825ba5ffbc06a2
I am the Carl <jamescarl96@gmail.com>
2020-04-22 02:16:57 -0400

    Added in several more method calls for the Rust machine encapsulation.

--- src/java_encapsulation.rs
+++ src/java_encapsulation.rs
@@ -194,7 +194,24 @@ pub mod open_computers {
     pub struct Machine<'a> {
         env: &'a JNIEnv<'a>,
         machine: JObject<'a>,
-        get_components_method_id: JMethodID<'a>
+        get_components_method_id: JMethodID<'a>,
+        component_count_id: JMethodID<'a>,
+        max_component_count_id: JMethodID<'a>,
+        get_cost_per_tick_id: JMethodID<'a>,
+        set_cost_per_tick_id: JMethodID<'a>,
+        get_tmp_address_id: JMethodID<'a>,
+        get_last_error_id: JMethodID<'a>,
+        get_world_time_id: JMethodID<'a>,
+        get_uptime_id: JMethodID<'a>,
+        get_cpu_time_id: JMethodID<'a>,
+        beep_id: JMethodID<'a>,
+        crash_id: JMethodID<'a>,
+        pop_signal_id: JMethodID<'a>,
+        methods_id: JMethodID<'a>,
+        invoke_id: JMethodID<'a>,
+        get_users_id: JMethodID<'a>,
+        add_user_id: JMethodID<'a>,
+        remove_user_id: JMethodID<'a>,
     }
     
     #[allow(dead_code)]
@@ -204,11 +221,45 @@ pub mod open_computers {
             let class = env.auto_local(env.find_class("li/cil/oc/api/machine/Machine")?);
 
             let get_components_method_id = env.get_method_id(&class, "components", "()Ljava/util/Map;")?;
+            let component_count_id = env.get_method_id(&class, "componentCount", "()I")?;
+            let max_component_count_id = env.get_method_id(&class, "maxComponents", "()I")?;
+            let get_cost_per_tick_id = env.get_method_id(&class, "getCostPerTick", "()D")?;
+            let set_cost_per_tick_id = env.get_method_id(&class, "setCostPerTick", "(D)V")?;
+            let get_tmp_address_id = env.get_method_id(&class, "tmpAddress", "()Ljava/lang/String;")?;
+            let get_last_error_id = env.get_method_id(&class, "lastError", "()Ljava/lang/String;")?;
+            let get_world_time_id = env.get_method_id(&class, "worldTime", "()J")?;
+            let get_uptime_id = env.get_method_id(&class, "upTime", "()D")?;
+            let get_cpu_time_id = env.get_method_id(&class, "cpuTime", "()D")?;
+            let beep_id = env.get_method_id(&class, "beep", "(SS)V")?;
+            let crash_id = env.get_method_id(&class, "crash", "(Ljava/lang/String;)Z")?;
+            let pop_signal_id = env.get_method_id(&class, "popSignal", "()Lli/cil/oc/api/machine/Signal;")?;
+            let methods_id = env.get_method_id(&class, "methods", "(Ljava/lang/Object;)Ljava/util/Map;")?;
+            let invoke_id = env.get_method_id(&class, "invoke", "(Ljava/lang/String;Ljava/lang/String;[Ljava/lang/Object;)[Ljava/lang/Object;")?;
+            let get_users_id = env.get_method_id(&class, "users", "()[Ljava/lang/String;")?;
+            let add_user_id = env.get_method_id(&class, "addUser", "(Ljava/lang/String;)V")?;
+            let remove_user_id = env.get_method_id(&class, "removeUser", "(Ljava/lang/String;)Z")?;
 
             Ok(Machine {
                 env,
                 machine,
                 get_components_method_id,
+                component_count_id,
+                max_component_count_id,
+                get_cost_per_tick_id,
+                set_cost_per_tick_id,
+                get_tmp_address_id,
+                get_last_error_id,
+                get_world_time_id,
+                get_uptime_id,
+                get_cpu_time_id,
+                beep_id,
+                crash_id,
+                pop_signal_id,
+                methods_id,
+                invoke_id,
+                get_users_id,
+                add_user_id,
+                remove_user_id,
             })
         }
     
@@ -227,72 +278,114 @@ pub mod open_computers {
             }
         }
     
-        pub fn component_count() -> u32 {
+        pub fn component_count(&self) -> JNIResult<u32> {
+            let result = self.env.call_method_unchecked(
+                self.machine,
+                self.component_count_id,
+                JavaType::Primitive(Primitive::Int),
+                &[]
+            )?;
+
+            Ok(result.i().unwrap() as u32)
+        }
+    
+        pub fn max_component_count(&self) -> JNIResult<u32> {
+            let result = self.env.call_method_unchecked(
+                self.machine,
+                self.max_component_count_id,
+                JavaType::Primitive(Primitive::Int),
+                &[]
+            )?;
+
+            Ok(result.i().unwrap() as u32)
+        }
+    
+        pub fn get_cost_per_tick(&self) -> JNIResult<f64> {
+            let result = self.env.call_method_unchecked(
+                self.machine,
+                self.component_count_id,
+                JavaType::Primitive(Primitive::Double),
+                &[]
+            )?;
+
+            Ok(result.d().unwrap())
+        }
+    
+        pub fn set_cost_per_tick(&self, price: f64) -> JNIResult<()>{
             unimplemented!()
         }
     
-        pub fn max_component_count() -> u32 {
+        pub fn get_tmp_address(&self) -> JNIResult<String> {
             unimplemented!()
         }
     
-        pub fn get_cost_per_tick() -> f64 {
+        pub fn get_last_error(&self) -> JNIResult<String> {
             unimplemented!()
         }
     
-        pub fn set_cost_per_tick(price: f64) {
+        pub fn get_world_time(&self) -> JNIResult<u64> {
+            let result = self.env.call_method_unchecked(
+                self.machine,
+                self.get_world_time_id,
+                JavaType::Primitive(Primitive::Int),
+                &[]
+            )?;
+
+            Ok(result.j().unwrap() as u64)
+        }
+    
+        pub fn get_uptime(&self) -> JNIResult<f64> {
+            let result = self.env.call_method_unchecked(
+                self.machine,
+                self.get_uptime_id,
+                JavaType::Primitive(Primitive::Int),
+                &[]
+            )?;
+
+            Ok(result.d().unwrap())
+        }
+    
+        pub fn get_cpu_time(&self) -> JNIResult<f64> {
+            let result = self.env.call_method_unchecked(
+                self.machine,
+                self.get_cpu_time_id,
+                JavaType::Primitive(Primitive::Int),
+                &[]
+            )?;
+
+            Ok(result.d().unwrap())
+        }
+    
+        pub fn beep(&self, frequency: u16, duration: u16) -> JNIResult<()> {
             unimplemented!()
         }
     
-        pub fn get_tmp_address() -> String {
+        pub fn crash(&self, message: &str) -> JNIResult<bool> {
             unimplemented!()
         }
     
-        pub fn get_last_error() -> String {
+        pub fn pop_signal(&self) -> JNIResult<Signal> {
             unimplemented!()
         }
     
-        pub fn get_world_time() -> u64 {
-            unimplemented!()
-        }
-    
-        pub fn get_uptime() -> f64 {
-            unimplemented!()
-        }
-    
-        pub fn get_cpu_time() -> f64 {
-            unimplemented!()
-        }
-    
-        pub fn beep(frequency: u16, duration: u16) {
-            unimplemented!()
-        }
-    
-        pub fn crash(message: &str) -> bool {
-            unimplemented!()
-        }
-    
-        pub fn pop_signal() -> Signal {
-            unimplemented!()
-        }
-    
-        pub fn methods(value: ValEnviron) -> Map<String, Callback> {
+        pub fn methods(&self, value: ValEnviron) -> JNIResult<Map<String, Callback>> {
             unimplemented!()
         }
 
-        pub fn invoke(address: &str, method: &str, args: &[MethodValue]) -> Result<Box<[MethodValue]>, ()> {
+        pub fn invoke(&self, address: &str, method: &str, args: &[MethodValue]) -> Result<Box<[MethodValue]>, ()> {
             // TODO remember to handle an exception correctly.
             unimplemented!()
         }
 
-        pub fn get_users() -> Box<[String]> {
+        pub fn get_users(&self) -> JNIResult<Box<[String]>> {
             unimplemented!()
         }
 
-        pub fn add_user(name: &str) -> Result<(), &str> {
+        pub fn add_user(&self, name: &str) -> JNIResult<()> {
             unimplemented!()
         }
 
-        pub fn remove_user(name: &str) -> bool {
+        pub fn remove_user(&self, name: &str) -> JNIResult<bool> {
             unimplemented!()
         }
     }
